638a3706fb17d98c3741f0dd804bd759c1cd6d74
Sardius <0xsardius@gmail.com>
2023-09-04 11:45:03 -0400

    Created card component and feedback list

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, temporibus earum? Magni officia quos eaque."
+
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, temporibus earum? Magni officia quos eaque."
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, temporibus earum? Magni officia quos eaque."
+    }
+] 
+    export default FeedbackData;
