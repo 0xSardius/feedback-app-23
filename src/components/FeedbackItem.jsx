728c6d0803cfb793956da3608d0eef6c18eb909e
Sardius <0xsardius@gmail.com>
2023-09-05 19:58:09 -0400

    Completed initial components, props and state

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -3,16 +3,12 @@ import PropTypes from "prop-types"
 import Card from "./shared/Card"
 
 
-function FeedbackItem({item}) {
-    const handleClick = (id) => {
-        console.log(id)
-    }
-
+function FeedbackItem({item, handleDelete}) {
 
     return (
         <Card reverse={true}>
             <div className="num-display">{item.rating}</div>
-            <button onClick={() => handleClick(item.id)} className="close">
+            <button onClick={() => handleDelete(item.id)} className="close">
                 <FaTimes color='purple'/>
             </button>
             <div className="text-display">
