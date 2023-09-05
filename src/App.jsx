2ddb7be55b734772ac82f3c2a6d4e88129034a6c
Sardius <0xsardius@gmail.com>
2023-09-05 06:19:56 -0400

    Adding in fa icons and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import {useState} from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from "./data/FeedbackData"
-import Card from "./components/shared/Card"
 
 function App() { 
 
-  const [feedback, setFeedback] = useState(FeedbackData)  
+  const [feedback, setFeedback] = useState(FeedbackData)
+
   return (
     <>
       <Header />
